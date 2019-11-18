d4b469d94e043bc3592c24178f940a4b17a195aa
nelkott <43351826+nelkott@users.noreply.github.com>
2019-11-18 13:07:27 -0500

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,3 +1,10 @@
 //Dictionary.java
 
-ArrayList<String> Dictionary = new ArrayList<String>();
+import java.util.ArrayList;
+
+public class Dictionary {
+
+    ArrayList<String> Dictionary = new ArrayList<String>();
+
+
+}
