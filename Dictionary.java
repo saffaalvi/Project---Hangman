504b62ee59ce6b4ccfb59d8382d2603075fb89b4
nelkott <43351826+nelkott@users.noreply.github.com>
2019-11-18 13:19:15 -0500

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,2 +1,45 @@
 //Dictionary.java
-//y
+
+import java.util.ArrayList;
+
+public class Dictionary {
+
+    ArrayList<String> easy = new ArrayList<String>();
+    ArrayList<String> medium = new ArrayList<String>();
+    ArrayList<String> hard = new ArrayList<String>();  
+    
+    // Medium ArrayList
+    // 6 words
+    medium.add("agency");
+    medium.add("airbag");
+    medium.add("alarms");
+    medium.add("cheque");
+    medium.add("jacket");
+    medium.add("honest");
+    medium.add("impair");
+    medium.add("junior");
+    medium.add("levels");
+    medium.add("moused");
+    // 7 words
+    medium.add("attacks");
+    medium.add("barrier");
+    medium.add("battled");
+    medium.add("choices");
+    medium.add("harness");
+    medium.add("laptops");
+    medium.add("lighter");
+    medium.add("tourism");
+    medium.add("unwound");
+    medium.add("voyager");
+    // 8 words
+    medium.add("bulletin");
+    medium.add("calendar");
+    medium.add("collapse");
+    medium.add("criminal");
+    medium.add("teaching");
+    medium.add("southern");
+    medium.add("proposal");
+    medium.add("momentum");
+    medium.add("organize");
+    medium.add("flexible");
+}
