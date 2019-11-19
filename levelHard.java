ea4698b4019d15017d10b375b5de169fb74ce662
saffaalvi <57377830+saffaalvi@users.noreply.github.com>
2019-11-19 14:08:47 -0500

    Update levelHard.java

--- levelHard.java
+++ levelHard.java
@@ -1,5 +1,11 @@
+import java.util.Random;
 
 public class levelHard extends Dictionary
 {
-
+	public levelHard()
+	{
+		Random rand = new Random();
+	    int rnd = rand.nextInt(hard.size());
+	    System.out.println(hard.get(rnd));
+	}
 }
