54cac8d438534b132980f07abd3acaa7fec21300
saffaalvi <57377830+saffaalvi@users.noreply.github.com>
2019-11-19 14:00:40 -0500

    Update levelMedium.java

--- levelMedium.java
+++ levelMedium.java
@@ -1,4 +1,14 @@
+import java.util.*;
 
-public class levelMedium extends Dictionary{
-
+public class levelMedium extends Dictionary
+{
+	public levelMedium()
+	{
+		for (int i = 0; i < medium.size(); i++) 
+	    {
+	        int rnd = new Random().nextInt(medium.size());
+	        System.out.println(medium.get(rnd));
+	    }
+	}
+    
 }
