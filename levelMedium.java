d0fbfa3388a14a9af375220425480a4096df2a4c
saffaalvi <57377830+saffaalvi@users.noreply.github.com>
2019-11-19 14:15:18 -0500

    Update levelMedium.java

--- levelMedium.java
+++ levelMedium.java
@@ -4,9 +4,12 @@ public class levelMedium extends Dictionary
 {
 	public levelMedium()
 	{
-		Random rand = new Random();
+	    Random rand = new Random();
 	    int rnd = rand.nextInt(medium.size());
-	    System.out.println(medium.get(rnd));
+	    String word = medium.get(rnd);
+	    System.out.println(word);
+	    for (int i=0; i<word.length(); i++)
+	    	System.out.print("_ ");
 	}
     
 }
