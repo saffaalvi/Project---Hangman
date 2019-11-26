78ec3dbb7c57d4a5399e8e067041beb2275a8d5b
saffaalvi <57377830+saffaalvi@users.noreply.github.com>
2019-11-26 14:56:42 -0500

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -14,22 +14,21 @@ public class Dictionary {
         easy.addAll(Arrays.asList(list));
 
     // Medium ArrayList
-   String[] list ={"agency","airbag","alarms","cheque","jacket","honest","impair","junior","levels","moused",
+   String[] list1 ={"agency","airbag","alarms","cheque","jacket","honest","impair","junior","levels","moused",
                 "attacks", "barrier", "battled", "choices", "harness", "laptops", "lighter", "tourism","unwound",
                 "voyager","bulletin","calendar","collapse","criminal","teaching","southern","proposal","momentum",
                 "organize","flexible"};
-        medium.addAll(Arrays.asList(list));
+        medium.addAll(Arrays.asList(list1));
     
     //hard
     //index 0 to 39
-    String list9[] = {"alchemist", "amplitude", "authorize", "awestruck", "binocular", "gymnastic", "hydration", "numerical", "nightmare", "supremacy"};
-    hard.addAll(Arrays.asList(list9));
-    String list10[] = {"academical", "biographer", "bibliology", "cassiopeia", "cryptogram", "elliptical", "hemisphere", "heliograph", "phenomenal", "philosophy"};
-    hard.addAll(Arrays.asList(list10));
-    String list11[] = {"accommodate", "affirmative", "blasphemous", "calligraphy", "designation", "evaporation", "exclamation", "holographic", "observatory", "seismograph"}; 
-    hard.addAll(Arrays.asList(list11));
-    String list12[] = {"alphabetical", "amphitheater", "catastrophic", "chromosphere", "evolutionist", "hieroglyphic", "hippopotamus", "intellectual", "photographer", "sophisticate"}; 
-    hard.addAll(Arrays.asList(list12));
+    String list2[] = {"alchemist", "amplitude", "authorize", "awestruck", "binocular", "gymnastic", "hydration", 
+    		"numerical", "nightmare", "supremacy", "academical", "biographer", "bibliology", "cassiopeia", "cryptogram", 
+    		"elliptical", "hemisphere", "heliograph", "phenomenal", "philosophy", "accommodate", "affirmative", "blasphemous", 
+    		"calligraphy", "designation", "evaporation", "exclamation", "holographic", "observatory", "seismograph", 
+    		"alphabetical", "amphitheater", "catastrophic", "chromosphere", "evolutionist", "hieroglyphic", "hippopotamus", 
+    		"intellectual", "photographer", "sophisticate"}; 
+    hard.addAll(Arrays.asList(list2));
 	} 
     public ArrayList<String> getEasy()
     {
@@ -44,39 +43,29 @@ public class Dictionary {
     	return hard;
     }
    
-	/**
 	//took me forever, also consider making wrong_guesses an attribute of whatever class
-	public void printHangman(){
-		int wrong_guesses=0;
-
+	public static void printHangman(int wrong_guesses){
 		if(wrong_guesses>=0){
 		    System.out.println("\t   --------");
 		    System.out.print("\t  |\n\t  |\n\t  |\n");
 		    wrong_guesses++;
 		}
-
 		if(wrong_guesses>=1){
 		    System.out.print("\t ---\n\t|   |\n\t ---\n");
 		    wrong_guesses+=3;
 		}
-
-
 		if(wrong_guesses==2){
 		     System.out.print("\t  |\n\t  |\n\t  |\n\t  |\n\t  |\n");
-
 		}
 		else if(wrong_guesses==3){
 		    System.out.print("\t  |\t/\n\t  |   /\n\t  | /\n\t  |\n\t  |\n");
-
 		}
 		 else if(wrong_guesses==4){
 		    System.out.print("      \\   |   /\n       \\  |  /\n\t\\ | /\n\t  |\n\t  |\n");
 		    wrong_guesses+=2;
 		}
-
 		if(wrong_guesses==5){
 		    System.out.println("\t    \\\n\t     \\\n\t      \\");
-
 		}
 		else if(wrong_guesses==6){
 		    System.out.println("\t/   \\\n       /     \\\n      /       \\");
@@ -84,5 +73,5 @@ public class Dictionary {
 		    //end game or something
 		}
 	}
-	*/
 }
+
