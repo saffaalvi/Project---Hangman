6f2619de2d5ba6086ff2b829da6d0fa23ad8bd1d
saffaalvi <57377830+saffaalvi@users.noreply.github.com>
2019-11-26 14:57:57 -0500

    Update test.java

--- test.java
+++ test.java
@@ -5,21 +5,7 @@ public class test{
 	public static void main(String[] args) {
 		
 		Dictionary temp = new Dictionary();
-		
-		
-		/**for (String str:temp.getEasy())
-		{
-			System.out.println(temp.getEasy().indexOf(str)+ " " + str.toString());
-		}
-	    for (String str:temp.getMed())
-		{
-			System.out.println(temp.getMed().indexOf(str)+ " " + str.toString());
-		}
-	    for (String str:temp.getHard())
-		{
-			System.out.println(temp.getHard().indexOf(str)+ " " + str.toString());
-		}**/
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter level (1 for easy, 2 for medium, 3 for hard): ");
 		int level = sc.nextInt();
