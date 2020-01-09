9154dbb3af8dc671cc3f27586684f149a50d368d
saffaalvi <57377830+saffaalvi@users.noreply.github.com>
2020-01-09 15:17:57 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,30 +1,23 @@
 import java.util.*;
-
-public class test{
-
-	public static void main(String[] args) {
-		
-		Dictionary temp = new Dictionary();
-
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter level (1 for easy, 2 for medium, 3 for hard): ");
-		int level = sc.nextInt();
-		if (level == 1)
-		{
-			levelEasy game = new levelEasy();
-		}
-		if (level == 2)
-		{
-			levelMedium game = new levelMedium();
-		}
-		if (level == 3)
-		{
-			levelHard game = new levelHard();
-		}
-			
-			
-			
-	}
-
-
+//this class starts the game, asks user for which level they would like to play
+public class test
+{
+    //main method
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter level (1 for easy, 2 for medium, 3 for hard): ");
+        int level = sc.nextInt();
+        if (level == 1)
+        {
+            game run = new game(1);
+        }
+        if (level == 2)
+        {
+            game run = new game(2);
+        }
+        if (level == 3) {
+            game run = new game(3);
+        }
+    }
 }
